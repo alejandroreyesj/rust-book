e47bce148afc12175cb96210007a64d2104b4e53
Alejandro R <alejandro.reyesj@outlook.com>
2023-02-25 15:46:39 -0500

    Automated Tests Chapter Part 1 and 2

---
+++ auto_tests/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Make this test fail")]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}
--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,6 @@
 pub mod generics_part_2_traits;
+use std::fmt::Display;
+
 use generics_part_2_traits::{NewsArticle, Summary};
 fn main() {
     // Generics Part 1
@@ -62,5 +64,17 @@ impl<'a> ImportantExcerpt<'a> {
     fn announce_and_return_part<'b>(&self, announcement: &'b str) -> &'b str {
         println!("Attention please: {announcement}");
         announcement
+    }
+}
+
+fn longest_with_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
