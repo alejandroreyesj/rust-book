687e2aff850bd02363d2c5a8c18e955f0ddd3790
Alejandro R <alejandro.reyesj@outlook.com>
2023-02-25 21:26:48 -0500

    Refactoring and error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args)?;
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    println!("With contents: {contents}");
+    Ok(())
+}
+
+struct Config<'a> {
+    query: &'a str,
+    file_path: &'a str,
+}
+
+impl<'a> Config<'a> {
+    fn build(args: &'a [String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        Ok(Self {
+            query: &args[1],
+            file_path: &args[2],
+        })
+    }
 }
