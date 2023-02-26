7305b644f581007e6b97d7b998cd7ee687aa2ad9
Alejandro R <alejandro.reyesj@outlook.com>
2023-02-25 21:32:57 -0500

    Remove compile files

--- minigrep/target/debug/deps/minigrep-05a4c146b994d28a.d
+++
@@ -1,8 +0,0 @@
-/home/alejandro/Documents/rust_projects/rust-book/minigrep/target/debug/deps/minigrep-05a4c146b994d28a.rmeta: src/main.rs Cargo.toml
-
-/home/alejandro/Documents/rust_projects/rust-book/minigrep/target/debug/deps/minigrep-05a4c146b994d28a.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- minigrep/target/debug/deps/minigrep-9c510f1dd9e22118.d
+++
@@ -1,8 +0,0 @@
-/home/alejandro/Documents/rust_projects/rust-book/minigrep/target/debug/deps/minigrep-9c510f1dd9e22118.rmeta: src/main.rs Cargo.toml
-
-/home/alejandro/Documents/rust_projects/rust-book/minigrep/target/debug/deps/minigrep-9c510f1dd9e22118.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
