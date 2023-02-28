a06609d944446ebd922822490cfc7ec30468b01c
Alejandro R <alejandro.reyesj@outlook.com>
2023-02-28 00:04:17 -0500

    Closures part 2

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,62 @@
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!("The user with a preference gets {giveaway1:?}");
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!("The user without a preference gets {giveaway2:?}");
+    {
+        let list = vec![1, 2, 3];
+        println!("Before defining closure: {list:?}");
+
+        let only_borrows = || println!("From closure: {list:?}");
+
+        println!("Before calling closure: {list:?}");
+        only_borrows();
+        println!("After calling closure: {list:?}");
+    }
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || list.push(7);
+
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+    fn most_stocked(&self) -> ShirtColor {
+        let mut red = 0;
+        let mut blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => red += 1,
+                ShirtColor::Blue => blue += 1,
+            }
+        }
+        if red > blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
 }
