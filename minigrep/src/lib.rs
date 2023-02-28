9be495f63c4e639726ee062da1bc9882a6f0ec0e
Alejandro R <alejandro.reyesj@outlook.com>
2023-02-27 20:44:07 -0500

    minigrep chapter done

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config<'a> {
     pub query: &'a str,
     pub file_path: &'a str,
+    ignore_case: bool,
 }
 
 impl<'a> Config<'a> {
@@ -14,33 +15,61 @@ impl<'a> Config<'a> {
         Ok(Self {
             query: &args[1],
             file_path: &args[2],
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
         })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let _contents = fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(config.file_path)?;
+    let results = if config.ignore_case {
+        search_case_insensitive(config.query, &contents)
+    } else {
+        search(config.query, &contents)
+    };
+    results.into_iter().for_each(|line| {
+        println!("{line}");
+    });
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
-        .split('\n')
+        .lines()
         .map(|line| line.trim())
-        .filter(|line| line.trim().contains(query))
+        .filter(|line| line.contains(query))
         .collect::<Vec<&str>>()
+}
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .map(|line| line.trim())
+        .filter(|line| line.contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
         Rust:
         safe, fast, productive.
         Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "DuCt";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
