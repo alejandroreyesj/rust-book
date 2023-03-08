d21c950dd69d489819ff6c03e3dbfa0b07065e1d
Alejandro R <alejandro.reyesj@outlook.com>
2023-03-08 00:31:54 -0500

    Channel examples

---
+++ channels/src/main.rs
@@ -0,0 +1,33 @@
+use std::{sync::mpsc, thread, time::Duration};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    // Example 1 Channels
+    // thread::spawn(move || {
+    //     let val = String::from("hello");
+    //     tx.send(val).unwrap();
+    //     // println!("{val}");
+    // });
+
+    // let received = rx.recv().unwrap();
+    // println!("Got: {received}")
+
+    // Example multivalue channels
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
+}
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time::Duration};
+use std::{thread, time::Duration, vec};
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -7,11 +7,19 @@ fn main() {
             thread::sleep(Duration::from_millis(1));
         }
     });
+    handle.join().unwrap();
 
     for i in 1..5 {
         println!("Hi number {i} from main thread");
         thread::sleep(Duration::from_millis(1));
     }
+    //Move using closures
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("here's a vector: {:?}", v);
+    });
 
-    handle.join().unwrap()
+    handle.join().unwrap();
+
+    // Channels
 }
