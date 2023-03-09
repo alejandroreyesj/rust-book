d11ea001c843bd8b1bce9521332806b3a23fab16
Alejandro R <alejandro.reyesj@outlook.com>
2023-03-08 20:06:53 -0500

    MPSC Channels

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -14,6 +14,24 @@ fn main() {
     // println!("Got: {received}")
 
     // Example multivalue channels
+    // let handle = thread::spawn(move || {
+    //     let vals = vec![
+    // String::from("hi"),
+    // String::from("from"),
+    // String::from("the"),
+    // String::from("thread"),
+    //     ];
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //     }
+    // });
+
+    // for received in rx {
+    //     println!("Got: {received}");
+    // }
+
+    // multiproducer by colining the transfmitter
+    let tx1 = tx.clone();
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -22,12 +40,25 @@ fn main() {
             String::from("thread"),
         ];
         for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
             tx.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
-
     for received in rx {
-        println!("Got: {received}");
+        println!("Got {received}!");
     }
+
+    // handle.join().unwrap();
 }
