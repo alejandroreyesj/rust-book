0cec42a48e76e846858948477b5b872f8da530f5
Alejandro R <alejandro.reyesj@outlook.com>
2023-03-30 20:31:57 -0400

    Rust Book macros

---
+++ macros/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world!");
+    let y = i_vec!(1, 2, 3);
+    println!("{y:?}")
+}
+
+#[macro_export]
+macro_rules! i_vec {
+    ($($x:expr), *) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+
+    };
+}
